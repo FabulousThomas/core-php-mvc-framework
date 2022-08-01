9933b793447086f7e31fc53543af61acce1261fe
Mba Thomas <angelfab631@gmail.com>
2022-08-01 01:46:42 +0100

    Update Controller.php

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,1 +1,18 @@
-<?
+<?php
+/*
+ * Base Controller
+ * Loads the models and views
+ */
+class Controller
+{
+   // Load Model
+   public function model($model)
+   {
+      // Require model
+      require_once '../app/models/' . $model . '.php';
+      // Instantiate model
+      return new $model;
+   }
+
+   
+}
