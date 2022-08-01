576ff686ccb1bf13f13781b67960acab13884b3f
Mba Thomas <angelfab631@gmail.com>
2022-08-01 02:05:12 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,3 +5,12 @@ define('DB_USER', 'YOUR_DATABASE_USERNAME');
 define('DB_PASS', 'YOUR_DATABASE_PASSWORD');
 define('DB_NAME', 'YOUR_DATABASE_NAME');
 
+// Site params
+// App root directory
+define('APPROOT', dirname(dirname(__FILE__)));
+// Url root / EXAMPLE: "https://github.com/FabulousThomas"
+define('URLROOT', 'YOUR_URL_ROOT');
+// Site Name
+define('SITENAME', 'YOUR_WEB_SITE_NAME');
+// App version
+define('APPVERSION', '1.0.0');
