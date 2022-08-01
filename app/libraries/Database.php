4fbb48028e03775d8f7240b8cfbbab91f06c5fe3
Mba Thomas <angelfab631@gmail.com>
2022-08-01 02:18:13 +0100

    Update Database.php

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -13,4 +13,22 @@ class Database {
    private $dbh;
    private $stmt;
    private $error;
+
+   public function __construct()
+   {
+      // Set DSN (Database String Name);
+      $dsn = 'mysql:host=' . $this->host . '; dbname=' . $this->dbname;
+      $options = array(
+         PDO::ATTR_PERSISTENT => true,
+         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      );
+
+      // Create PDO instance
+      try {
+         $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+      } catch (PDOException $e) {
+         $this->error = $e->getMessage();
+         echo $this->error;
+      }
+   }
 }
