ed40af23247efc918ddee5d4020ad7d9c5298318
Mba Thomas <angelfab631@gmail.com>
2022-08-05 23:29:32 +0100

    Update Database.php

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -29,7 +29,7 @@ class Database
          $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
       } catch (PDOException $e) {
          $this->error = $e->getMessage();
-         echo $this->erro
+         echo $this->error;
       }
    }
 
