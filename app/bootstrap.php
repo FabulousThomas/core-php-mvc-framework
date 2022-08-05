bb091d5e09d199846ff4dbded7ee2601d108ca0d
Mba Thomas <angelfab631@gmail.com>
2022-08-05 23:37:57 +0100

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,3 +1,8 @@
 <?php 
 // Load config file
 require_once 'config/config.php';
+
+// Load Classes
+spl_autoload_register(function($className) {
+   require_once 'libraries/' . $className . '.php';
+});
