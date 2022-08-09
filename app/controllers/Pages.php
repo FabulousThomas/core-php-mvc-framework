19c9b67e54800131c09408c51a180033aa40ffc6
Mba Thomas <angelfab631@gmail.com>
2022-08-10 00:10:12 +0100

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -13,11 +13,4 @@ class Pages extends Controller {
       $this->view('pages/index', $data);
    }
 
-   public function about() {
-      $data = [
-         'title' => 'About Us',
-         'description' => 'About Page'
-      ];
-      $this->view('pages/about', $data);
-   }
 }
