93a5416e9858ef022d099bd0031d591b1b81d63c
Mba Thomas <angelfab631@gmail.com>
2022-08-11 04:32:41 +0100

    Update index.php

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,13 @@
+<?php require APPROOT . '/views/inc/head.php'?>
 
-<h1><?php echo $data['title'] ?></h1>
-<h3><?php echo $data['description'] ?></h3>
+
+<div class="container">
+<div class="jumbotron jumbotron-fluid text-center">
+   <div class="container">
+      <h1 class="text-uppercase"><?php echo $data['title'] ?></h1>
+      <p class="lead"><?php echo $data['description'] ?></p>
+   </div>
+</div>
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php'?>
