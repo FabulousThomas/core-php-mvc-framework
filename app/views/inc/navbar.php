1efbc8c064e293a10d70a48b9817a16702348120
Mba Thomas <angelfab631@gmail.com>
2022-08-11 05:30:40 +0100

    Create navbar.php

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,27 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+<div class="container">
+    <a class="navbar-brand" href="<?php echo URLROOT; ?>/index">SITE LOGO</a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+        aria-expanded="false" aria-label="Toggle navigation"><i class="las la-qrcode"></i></button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT; ?>/index">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
+
