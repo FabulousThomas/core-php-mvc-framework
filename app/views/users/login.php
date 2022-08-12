fba91668f867f5522e28b19ec7c91444928d593b
Mba Thomas <angelfab631@gmail.com>
2022-08-12 06:40:15 +0100

    Update login.php

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,13 +1,37 @@
-<?php require APPROOT . '/views/inc/head.php'?>
-<?php require APPROOT . '/views/inc/navbar.php'?>
+<?php require APPROOT . '/views/inc/head.php' ?>
+<?php require APPROOT . '/views/inc/navbar.php' ?>
 
-<div class="container">
-<div class="jumbotron jumbotron-fluid py-2 text-center">
-   <div class="container">
-      <h1 class="text-uppercase"><?php echo $data['title'] ?></h1>
-      <p class="lead mb-2"><?php echo $data['description'] ?></p>
+<div class="container mt-3">
+   <div class="jumbotron jumbotron-fluid py-2 text-center">
+      <h1 class="text-uppercase mb-0"><?php echo $data['title'] ?></h1>
+      <p class="lead mb-0"><?php echo $data['description'] ?></p>
+   </div>
+
+   <div class="card card-body px-0 col-lg-6 col-md-12 m-auto shadow border-0 rounded-0">
+      <div class="container">
+         <form action="<?php echo URLROOT; ?>/users/login" method="POST" enctype="multipart/form-data">
+         <?php flash_msg('register_msg'); ?>
+            <div class="form-group mb-3">
+               <label for="username">Username</label>
+               <input type="text" class="form-control <?php echo (!empty($data['username_err'])) ? 'is-invalid' : ''; ?> rounded-0" name="username" placeholder="Enter Username" value="<?php echo $data['username'] ?>">
+               <small class="invalid-feedback"><?php echo $data['username_err'] ?></small>
+            </div>
+            <div class="form-group mb-3">
+               <label for="password">Password</label>
+               <input type="password" class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?> rounded-0" name="password" placeholder="Enter Password" value="<?php echo $data['password'] ?>">
+               <small class="invalid-feedback"><?php echo $data['password_err'] ?></small>
+            </div>
+            <div class="form-group row">
+               <div class="col">
+                  <button type="submit" class="btn btn-primary form-control">Login</button>
+               </div>
+               <div class="col">
+                  <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-light form-control">Or Register</a>
+               </div>
+            </div>
+         </form>
+      </div>
    </div>
 </div>
-</div>
 
-<?php require APPROOT . '/views/inc/footer.php'?>
+<?php require APPROOT . '/views/inc/footer.php' ?>
