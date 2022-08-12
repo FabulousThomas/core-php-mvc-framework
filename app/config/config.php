19955c8e777b87863483070284e4eacccf1c619f
Mba Thomas <angelfab631@gmail.com>
2022-08-12 06:39:35 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,14 @@
 <?php 
 // Define database params
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'YOUR_DATABASE_USERNAME');
+// define('DB_PASS', 'YOUR_DATABASE_PASSWORD');
+// define('DB_NAME', 'YOUR_DATABASE_NAME');
+
 define('DB_HOST', 'localhost');
-define('DB_USER', 'YOUR_DATABASE_USERNAME');
-define('DB_PASS', 'YOUR_DATABASE_PASSWORD');
-define('DB_NAME', 'YOUR_DATABASE_NAME');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
 
 // Site params
 // App root directory
