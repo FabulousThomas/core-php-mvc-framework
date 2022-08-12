16c39febf5262408d84e9ad03f6e9b29b63cf579
Mba Thomas <angelfab631@gmail.com>
2022-08-12 07:52:27 +0100

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -9,20 +9,41 @@
 class User
 {
    private $db;
+   
    public function __construct()
    {
       $this->db = new Database;
       $this->createDatabe();
+      $this->createTable();
    }
 
+   // Create Database
    public function createDatabe() {
-      $this->db->query('CREATE DATABASE IF NOT EXISTS phpmvc');
+      $this->db->query('CREATE DATABASE IF NOT EXISTS `coremvc`');
    
       if($this->db->execute()) {
          return true;
       } else {
          return false;
       }
+   }
+
+   // Create Table
+   public function createTable() {
+      $this->db->query('CREATE TABLE IF NOT EXISTS `coremvc`.`users` ( 
+         `id` INT NOT NULL AUTO_INCREMENT , 
+         `name` VARCHAR(255) NOT NULL , 
+         `username` VARCHAR(255) NOT NULL , 
+         `email` VARCHAR(255) NOT NULL , 
+         `password` VARCHAR(255) NOT NULL , 
+         `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+         PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+
+         if($this->db->execute()) {
+            return true;
+         } else {
+            return false;
+         }
    }
 
    public function register($data)
