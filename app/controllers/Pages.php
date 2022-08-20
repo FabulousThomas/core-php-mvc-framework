b78b698a4079d144da47aa191d279aea65d78d2e
Mba Thomas <angelfab631@gmail.com>
2022-08-20 07:53:12 +0100

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,23 +1,48 @@
-<?php 
-class Pages extends Controller {
+<?php
+class Pages extends Controller
+{
+   private $userModel = '';
+   // private $data = [];
+   // private $username = '';
    public function __construct()
    {
-      
+      /* 
+       * redirects users to login page
+       * if not logged in
+       */
+      // if (!isLoggedInUser()) {
+      //    redirect('pages/success');
+      // }
+      $this->userModel = $this->model('User');
    }
 
-   public function index() {
+   public function index()
+   {
+      if(isLoggedInUser()) {
+         redirect('pages/success');
+      }
       $data = [
-         'title' => 'Core PHP MVC Framework',
-         'description' => 'A simple open source Core PHP MVC Framework'
+         'title' => 'Core PHP-MVC-Framework',
+         'description' => 'A simple open source PHP-MVC-Framework'
       ];
       $this->view('pages/index', $data);
    }
 
-   public function about() {
+   public function about()
+   {
       $data = [
          'title' => 'About this Framework',
          'description' => 'About Page'
       ];
       $this->view('pages/about', $data);
+   }
+
+   public function success()
+   {
+      $data = [
+         'title' => 'Login Success Page',
+         'description' => 'Success Page'
+      ];
+      $this->view('pages/success', $data);
    }
 }
