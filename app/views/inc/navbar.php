344d93f1afc072b089ac6bb1e068cd186cfdc394
Mba Thomas <angelfab631@gmail.com>
2022-08-20 01:14:26 +0100

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,27 +1,33 @@
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container">
-    <a class="navbar-brand" href="<?php echo URLROOT; ?>/index">SITE LOGO</a>
-    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation"><i class="las la-qrcode"></i></button>
-    <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/index">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
-            </li>
-        </ul>
-    </div>
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>/index">SITE LOGO</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"><i class="las la-qrcode"></i></button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/index">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <?php if (isset($_SESSION['email'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><?php echo  $_SESSION['email']?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
 </nav>
-
