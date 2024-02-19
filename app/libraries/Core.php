e893700554db5777c97e129d9192c53ee04e7e31
Mba Thomas <angelfab631@gmail.com>
2024-02-19 03:35:59 +0300

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,13 +10,18 @@ class Core
    private $currentMethod = 'index';
    private $params = [];
 
+   /**
+    * Get the URL and set the controller and method
+    *
+    * @return void
+    */
    public function __construct()
    {
       $url = $this->getUrl();
 
       if ($url) {
          // Look in controller for first value
-         if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+         if (file_exists(APPROOT . '/controllers/' . ucwords($url[0]) . '.php')) {
             // if file exists, set a controller
             $this->currentController = ucwords($url[0]);
             // unset 0 index
@@ -25,7 +30,7 @@ class Core
       }
 
       // Require a controller
-      require_once '../app/controllers/' . $this->currentController . '.php';
+      require_once APPROOT . '/controllers/' . $this->currentController . '.php';
       // Instantiate controller
       $this->currentController = new $this->currentController;
 
@@ -44,6 +49,11 @@ class Core
       call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
    }
 
+   /**
+    * Get the URL and parse it
+    *
+    * @return array|bool The URL segments or false if no URL is present
+    */
    public function getUrl()
    {
       if (isset($_GET['url'])) {
@@ -52,5 +62,51 @@ class Core
          $url = explode('/', $url);
          return $url;
       }
+
+      return false;
    }
+
+   // public function __construct()
+   // {
+   //    $url = $this->getUrl();
+
+   //    if ($url) {
+   //       // Look in controller for first value
+   //       if (file_exists(APPROOT . '/controllers/' . ucwords($url[0]) . '.php')) {
+   //          // if file exists, set a controller
+   //          $this->currentController = ucwords($url[0]);
+   //          // unset 0 index
+   //          unset($url[0]);
+   //       }
+   //    }
+
+   //    // Require a controller
+   //    require_once APPROOT . '/controllers/' . $this->currentController . '.php';
+   //    // Instantiate controller
+   //    $this->currentController = new $this->currentController;
+
+   //    // Look in controller for second value
+   //    if (isset($url[1])) {
+   //       // Check if method exists in controller
+   //       if (method_exists($this->currentController, $url[1])) {
+   //          $this->currentMethod = $url[1];
+   //          unset($url[1]);
+   //       }
+   //    }
+
+   //    // Get params
+   //    $this->params = $url ? array_values($url) : [];
+   //    // Call a callback with arrays of params
+   //    call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+   // }
+
+   // public function getUrl()
+   // {
+   //    if (isset($_GET['url'])) {
+   //       $url = rtrim($_GET['url'], '/');
+   //       $url = filter_var($url, FILTER_SANITIZE_URL);
+   //       $url = explode('/', $url);
+   //       return $url;
+   //    }
+   // }
 }
