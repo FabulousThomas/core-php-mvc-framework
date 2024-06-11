51633e41aa0e4ad08b16ee9e779ad72a4a4b7b21
Mba Thomas <angelfab631@gmail.com>
2024-06-11 21:12:23 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,6 @@
-<?php 
-// Define database params
+<?php
+
+// DATABASE PARAMS
 // define('DB_HOST', 'localhost');
 // define('DB_USER', 'YOUR_DATABASE_USERNAME');
 // define('DB_PASS', 'YOUR_DATABASE_PASSWORD');
@@ -10,7 +11,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'coremvc');
 
-// Site params
+// SITE PARAMS
 // App root directory
 define('APPROOT', dirname(dirname(__FILE__)));
 // Url root / EXAMPLE: "https://github.com/FabulousThomas"
@@ -19,3 +20,24 @@ define('URLROOT', 'http://localhost/core-php-mvc-framework');
 define('SITENAME', 'YOUR_WEB_SITE_NAME');
 // App version
 define('APPVERSION', '1.0.0');
+
+// CHECK CONNECTION
+
+if (!$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS)) {
+   die('UNABLE TO COONECT');
+}
+
+// CREATE DATABASE AND TABLES
+if ($conn->query('CREATE DATABASE IF NOT EXISTS `coremvc`')) {
+
+   $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+   $conn->query('CREATE TABLE IF NOT EXISTS `coremvc`.`users` ( 
+      `id` INT NOT NULL AUTO_INCREMENT, 
+      `name` VARCHAR(255) NOT NULL, 
+      `username` VARCHAR(255) NOT NULL, 
+      `email` VARCHAR(255) NOT NULL, 
+      `password` VARCHAR(255) NOT NULL, 
+      `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+      PRIMARY KEY (`id`)) ENGINE = InnoDB;');
+}
