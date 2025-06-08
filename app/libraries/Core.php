d3b2ba9aab856fcf8db7676e2e01d4796436319d
Mba Thomas <angelfab631@gmail.com>
2025-06-08 08:39:46 -0600

    Update Core.php
    Updated for better improvement

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,63 +6,66 @@
  */
 class Core
 {
-   private $currentController = 'Index'; // Default controller, can be changed to any existing controller
-   private $currentMethod = 'Index';
+   private $defaultController = 'Index';
+   private $defaultMethod = 'Index';
+   private $currentController;
+   private $currentMethod;
    private $params = [];
 
-   /**
-    * Get the URL and set the controller and method
-    *
-    * @return void
-    */
    public function __construct()
    {
+      $this->currentController = $this->defaultController;
+      $this->currentMethod = $this->defaultMethod;
+
       $url = $this->getUrl();
 
       if ($url) {
-         // Look in controller for first value
-         if (file_exists(APPROOT . '/controllers/' . ucwords($url[0]) . '.php')) {
-            // if file exists, set a controller
-            $this->currentController = ucwords($url[0]);
-            // unset 0 index
-            unset($url[0]);
+         // Validate and load controller
+         if (isset($url[0]) && $this->isValidName($url[0])) {
+            $controllerFile = APPROOT . '/controllers/' . ucwords($url[0]) . '.php';
+            if (file_exists($controllerFile)) {
+               $this->currentController = ucwords($url[0]);
+               unset($url[0]);
+            } else {
+               throw new Exception("Controller file not found: " . $controllerFile);
+            }
+         }
+
+         // Instantiate controller
+         require_once APPROOT . '/controllers/' . $this->currentController . '.php';
+         $this->currentController = new $this->currentController;
+
+         // Validate and set method
+         if (isset($url[1]) && $this->isValidName($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+               $this->currentMethod = $url[1];
+               unset($url[1]);
+            } else {
+               throw new Exception("Method {$url[1]} not found in controller {$this->currentController}");
+            }
          }
       }
 
-      // Require a controller
-      require_once APPROOT . '/controllers/' . $this->currentController . '.php';
-      // Instantiate controller
-      $this->currentController = new $this->currentController;
-
-      // Look in controller for second value
-      if (isset($url[1])) {
-         // Check if method exists in controller
-         if (method_exists($this->currentController, $url[1])) {
-            $this->currentMethod = $url[1];
-            unset($url[1]);
-         }
-      }
-
-      // Get params
+      // Set parameters
       $this->params = $url ? array_values($url) : [];
-      // Call a callback with arrays of params
-      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+
+      // Call method
+      $this->currentController->{$this->currentMethod}(...$this->params);
    }
 
-   /**
-    * Get the URL and parse it
-    *
-    * @return array|bool The URL segments or false if no URL is present
-    */
+   private function isValidName($name)
+   {
+      return preg_match('/^[a-zA-Z0-9_]+$/', $name);
+   }
+
    public function getUrl()
    {
       if (isset($_GET['url'])) {
          $url = rtrim($_GET['url'], '/');
          $url = filter_var($url, FILTER_SANITIZE_URL);
-         $url = explode('/', $url);
-         return $url;
+         return explode('/', $url);
       }
 
-      return false;
+      return [];
    }
 }
