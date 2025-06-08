c5fc856559100f3afa460269dad7beff1aded986
Mba Thomas <angelfab631@gmail.com>
2025-06-08 09:29:50 -0600

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,11 +1,14 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo URLROOT; ?>/index">SITE LOGO</a>
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>">SITE LOGO</a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"><i class="las la-qrcode"></i></button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/index">Home</a>
+                    <a class="nav-link" href="<?php echo URLROOT; ?>">Intro</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/docs">Docs</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
